8b709c3194b239a0507fccda8b4ae4aa6d05c8a0
iceynano <34570144+iceynano@users.noreply.github.com>
2024-07-09 06:35:02 +0800

    Update: now support other resolution

--- README.md
+++ README.md
@@ -6,16 +6,19 @@ Fisrt check your settings page, it should looks like below:
 
 By the way, my desktop resolution is 1920x1080, so if you could just use the resolution👈
 
+now script support other resolution, but **1080p** and settings below is still better, script now need time to initiate zoom factor to adapt window, so you may find first game would fail: it's ok, next game would be fine, or you can handle to exit fishing manually then reload.
+
 ![settings](https://github.com/iceynano/nikke_fishing/blob/main/settings.png)
 
 **NOTICE: DO NOT MODIFY GAME RESOLUTION WHEN CODE RUNNING**
 
 ## Steps
 
-1. ~~install requirements, I forgot to add a list or requirements.txt, anyway, just get the point.~~ Done.  
-now just running `pip install -r requirements.txt` (if you use venv or other virtual env, I bet you are good at this)
+1. if you want install manually, just running `pip install -r requirements.txt`. (if you use venv or other virtual env, I bet you are good at this)  
 
-2. run the py file **AS ADMIN**, I guess it would be fine if you install requirements properly.
+if you use release package, just unzip it then double click `start script.bat`, it would auto install requirements to folder under script.
+
+2. run the py file **AS ADMIN** if you install manually, I guess it would be fine if you install requirements properly.
 
 3. go to the fishing area and click twice button, script would help you press ⬆⬆⬇⬇⬅⬅➡➡ (only this! do not expect more.)
 
@@ -23,7 +26,7 @@ now just running `pip install -r requirements.txt` (if you use venv or other vir
 
 5. I guess nothing.
 
-6. If you find code runs badly, I have left extra funcs in script, fork and modify code as you like, I rarely read issues. (For long response time, try moddify number in [L212](https://github.com/iceynano/nikke_fishing/blob/49fff960ba15425bf55e11813b8592bfd3c8e64d/run.py#L212))
+6. If you find code runs badly, I have left extra funcs in script, fork and modify code as you like, I rarely read issues. (For long response time, try moddify number in [L212](https://github.com/iceynano/nikke_fishing/blob/49fff960ba15425bf55e11813b8592bfd3c8e64d/run.py#L287))
 
 7. USE AT YOUR OWN RISK, if you got banned, I would be glad to advise you play games that don't constrain the use of scripts. 
 
--- run.py
+++ run.py
@@ -3,11 +3,11 @@ import numpy as np
 import pyautogui
 import win32gui
 import time
-import random
 import copy
 from PIL import Image
 import keyboard
-import win32process
+
+GLSCALE = 1
 
 class Timer():
     last_time: float
@@ -45,75 +45,62 @@ def find_window_by_title(title):
     win32gui.EnumWindows(callback, hwnd_list)
     return hwnd_list[0] if hwnd_list else None
 
-def find_window_by_process(process_name):
-    hwnd_list = []
-    def callback(hwnd, hwnd_list):
-        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != "":
-            _, pid = win32process.GetWindowThreadProcessId(hwnd)
-            try:
-                _, found_process_name = win32process.GetModuleFileNameEx(pid, 0)
-            except win32process.error:
-                found_process_name = None
-            if found_process_name and found_process_name.lower() == process_name.lower():
-                hwnd_list.append(hwnd)
-        return True
-    win32gui.EnumWindows(callback, hwnd_list)
-    return hwnd_list[0] if hwnd_list else None
+# def find_window_by_process(process_name):
+#     # didn't work. need to adjust.
+#     hwnd_list = []
+#     def callback(hwnd, hwnd_list):
+#         if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != "":
+#             _, pid = win32process.GetWindowThreadProcessId(hwnd)
+#             try:
+#                 _, found_process_name = win32process.GetModuleFileNameEx(pid, 0)
+#             except win32process.error:
+#                 found_process_name = None
+#             if found_process_name and found_process_name.lower() == process_name.lower():
+#                 hwnd_list.append(hwnd)
+#         return True
+#     win32gui.EnumWindows(callback, hwnd_list)
+#     return hwnd_list[0] if hwnd_list else None
 
 def capture_window(hwnd):
     rect = win32gui.GetWindowRect(hwnd)
     left, top, right, bottom = rect
-    width = right - left
-    height = bottom - top
-
     screenshot = pyautogui.screenshot()
 
     img = screenshot.crop((left, top, right, bottom))
     
-    return img #, screenshot
+    return img
 
-def match_template(window_img, template_img, threshold=0.6):
-    window_img_cv = cv2.cvtColor(np.array(window_img), cv2.COLOR_RGB2BGR)
-    template_img_cv = cv2.cvtColor(np.array(template_img), cv2.COLOR_RGB2BGR)
-    
-    res = cv2.matchTemplate(window_img_cv, template_img_cv, cv2.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    
-    if max_val >= threshold:
-        return max_loc, template_img_cv.shape[1], template_img_cv.shape[0], max_val
-    else:
-        return None, None, None, max_val
-    
-def ticknow():
-    st = round(time.time(), 3)
-    return st
+def slowprt(now, id: int, interval: int, message: str):
+    if time.time() - now(id) >= interval:
+        print(message)
+        now(id, True)
 
-def click_random_in_region(button, tick, sig):
-    if time.time() - tick('press') >= 0.01:
-        rect = (0, 0)# win32gui.GetWindowRect(button.hwnd)
-        abs_x = rect[0] + button.top_left[0] + random.randint(0, button.width - 1)
-        abs_y = rect[1] + button.top_left[1] + random.randint(0, button.height - 1)
-        pyautogui.click(abs_x, abs_y)
-        tick('press', True)
-        print(f"Click @{sig} ({abs_x - rect[0]} {abs_y - rect[1]})")
+def newcrop(img: Image.Image, button: Button):
+    new_image = img.crop((button.top_left[0], button.top_left[1], button.top_left[0] + button.width, button.top_left[1] + button.height))
+    return new_image
 
-def newpress(sig, tick: Timer, hwnd):
-    if time.time() - tick('press') >= 0.2:
-        keyboard.press(sig)
-        keyboard.release(sig)
-        print(f"{sig} pressed!")
-        tick('press', True)
+def scale_try(image, template):
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    template = cv2.cvtColor(np.array(template), cv2.COLOR_RGB2BGR)
+    template_height, template_width = template.shape[:2]
 
-def appear_then_press(template: Image.Image, img: Image.Image, button: Button, sig, tick):
-    tl, *_ = match_template(img, template)
-    if tl:
-        newpress(sig, tick, button.hwnd)
-        # click_random_in_region(button, tick, sig)
+    scale_start = 0.5
+    scale_end = 2.0
+    scale_step = 0.1
 
-def ergotic(templates, img, button, tick):
-    sigs = ['left', 'right', 'up', 'down']
-    for i, template in enumerate(templates):
-        appear_then_press(template, img, button, sigs[i%4], tick)
+    best_scale = None
+    best_val = float('inf')
+
+    for scale in np.arange(scale_start, scale_end, scale_step):
+        scaled_template = cv2.resize(template, (int(round(template_width * scale, 0)), int(round(template_height * scale, 0))))
+        result = cv2.matchTemplate(image, scaled_template, cv2.TM_SQDIFF_NORMED)
+        min_val, _, min_loc, _ = cv2.minMaxLoc(result)
+        
+        if min_val < best_val:
+            best_val = min_val
+            best_scale = scale
+
+    return best_scale
      
 def id_timer():
     # return id's last use time
@@ -131,14 +118,70 @@ def id_timer():
 
     return timer
 
-def slowprt(now, id: int, interval: int, message: str):
-    if time.time() - now(id) >= interval:
-        print(message)
-        now(id, True)
+def match_template(window_img, template_img, threshold=0.6):
+    window_img_cv = cv2.cvtColor(np.array(window_img), cv2.COLOR_RGB2BGR)
+    if GLSCALE == 1:
+        template_img_cv = cv2.cvtColor(np.array(template_img), cv2.COLOR_RGB2BGR)
+    else:
+        template_img_cv = template_img
+    res = cv2.matchTemplate(window_img_cv, template_img_cv, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    
+    if max_val >= threshold:
+        return max_loc, template_img_cv.shape[1], template_img_cv.shape[0], max_val
+    else:
+        return None, None, None, max_val
 
-def newcrop(img: Image.Image, button: Button):
-    new_image = img.crop((button.top_left[0], button.top_left[1], button.top_left[0] + button.width, button.top_left[1] + button.height))
-    return new_image
+def scale_template(template, scale):
+    template = cv2.cvtColor(np.array(template), cv2.COLOR_RGB2BGR)
+    template_height, template_width = template.shape[:2]
+    scaled_template = cv2.resize(template, (int(round(template_width * scale, 0)), int(round(template_height * scale, 0))))
+    return scaled_template
+
+def cv_confirm(button: Button, flag: bool, img, template):
+    # determine whether need to scale templates
+    # stupid code, I have no better idea
+    global GLSCALE
+    if flag and button.confi >= 0.4:
+        if button.confi <= 0.7 and GLSCALE == 1:
+            # :if button not far from template, or it just didn't show in window
+            temp_scale = scale_try(img, template)
+            if temp_scale >= 0.7 and round(temp_scale, 1) != 1:
+                # scale 
+                GLSCALE = round(temp_scale, 2)
+                test_value = match_template(img, scale_template(template, temp_scale))
+                # print(GLSCALE, test_value[3])
+                if test_value[3] <= 0.7:
+                    GLSCALE = 1
+        else:
+            flag = False
+    return flag
+
+# def click_random_in_region(button, tick, sig):
+#     if time.time() - tick('press') >= 0.01:
+#         rect = win32gui.GetWindowRect(button.hwnd)
+#         abs_x = rect[0] + button.top_left[0] + random.randint(0, button.width - 1)
+#         abs_y = rect[1] + button.top_left[1] + random.randint(0, button.height - 1)
+#         pyautogui.click(abs_x, abs_y)
+#         tick('press', True)
+#         print(f"Click @{sig} ({abs_x - rect[0]} {abs_y - rect[1]})")
+
+def newpress(sig, tick: Timer, hwnd):
+    if time.time() - tick('press') >= 0.2:
+        keyboard.press(sig)
+        keyboard.release(sig)
+        print(f"{sig} pressed!")
+        tick('press', True)
+
+def appear_then_press(template: Image.Image, img: Image.Image, button: Button, sig, tick):
+    tl, _, _, confi = match_template(img, template)
+    if tl:
+        newpress(sig, tick, button.hwnd)
+
+def ergotic(templates, img, button, tick):
+    sigs = ['left', 'right', 'up', 'down']
+    for i, template in enumerate(templates):
+        appear_then_press(template, img, button, sigs[i%4], tick)
 
 def button_eval(button: Button, nan: tuple):
     if nan[0]:
@@ -147,9 +190,9 @@ def button_eval(button: Button, nan: tuple):
         button.height = nan[2]
     button.confi = nan[3]
 
-def button_stable(button: Button, img: Image.Image, template: Image.Image):
-    # judge if button location is stable
-    top_left, *_ = match_template(img, template)
+def button_stable(button: Button, img: Image.Image, template: Image.Image, threshold = 0.6):
+    # determine if button location is stable
+    top_left, *_ = match_template(img, template, threshold)
     if not top_left:
         return False
     if top_left != button.top_left:
@@ -173,19 +216,32 @@ def button_lock_on(button: Button, img: Image.Image, template: Image.Image, hwnd
             button.show, _, _, button.confi = match_template(crop_window, template)
 
 def fishing(hwnd, templates, threshold = 0.5):
+
+    def fl(file):
+        return 'assets\\' + file
+
     mask_image = [
-        Image.open(img)
+        Image.open(fl(img))
         for img in templates[0]
     ]
     icons = templates[1] + templates[2]
     icon_image = [
-        Image.open(img)
+        Image.open(fl(img))
         for img in icons
     ]
+
+    cv = False
+    # flag for templates whether converted
     flag = False
+    # flag for determine whether in fishing
+    need_cv = True
+    # flag for scale template
     now = id_timer()
+    # tool to debug time cost
     init = Timer()
+    # determine if fishing time out
     init.last_time = -40
+
     idfy_area = Button()
     buffer_area = Button()
     pause = Button()
@@ -195,6 +251,23 @@ def fishing(hwnd, templates, threshold = 0.5):
         if not hwnd:
             break
 
+        if GLSCALE != 1 and not cv:
+            # template broken detected, now scale templates
+            cv_mask_image = [
+                scale_template(template, GLSCALE)
+                for template in mask_image
+            ]
+            mask_image.clear()
+            mask_image = copy.copy(cv_mask_image)
+
+            cv_icon_image = [
+                scale_template(template, GLSCALE)
+                for template in icon_image
+            ]
+            icon_image.clear()
+            icon_image = copy.copy(cv_icon_image)
+            cv = True
+
         window_img = capture_window(hwnd)
 
         button_lock_on(endtime, window_img, mask_image[2], hwnd)
@@ -203,17 +276,22 @@ def fishing(hwnd, templates, threshold = 0.5):
 
         button_lock_on(pause, window_img, mask_image[0], hwnd)
 
+        need_cv = cv_confirm(pause, need_cv, window_img, mask_image[0])
+
         if pause.stable and pause.show:
             if not flag:
-                slowprt(now, 1, 2, f"Find area: {pause.top_left}, Width: {pause.width}, Hight: {pause.height}, Fishing Game Detected")
+                slowprt(now, 1, 5, f"Find area: {pause.top_left}, Width: {pause.width}, Hight: {pause.height}, Fishing Game Detected")
 
             button_lock_on(idfy_area, window_img, mask_image[1], hwnd)
 
             if idfy_area.stable and not buffer_area.stable:
                 # modify for screenshot latency, press latency, etc.
                 buffer_area = copy.copy(idfy_area)
-                buffer_area.top_left = (idfy_area.top_left[0] - 0, idfy_area.top_left[1])
-                buffer_area.width = idfy_area.width + 0
+                if buffer_area.stable:
+                    buffer_area.top_left = (idfy_area.top_left[0] - 0, idfy_area.top_left[1])
+                    buffer_area.width = idfy_area.width - 0
+        
+            need_cv = cv_confirm(idfy_area, need_cv, window_img, mask_image[1])
 
             if idfy_area.show or flag:
                 # 1. game started 2. game in progress
@@ -226,12 +304,16 @@ def fishing(hwnd, templates, threshold = 0.5):
                 if flag:
                     if time.time() - init.last_time <= 30:
                         crop_buffer = newcrop(window_img, buffer_area)
-                        ergotic(icon_image, crop_buffer, idfy_area, now)
+                        ergotic(icon_image, crop_buffer, buffer_area, now)
                     else:
                         # now timeout, exit process
+                        print(">>>>>>>>>>>>>>>>Fishing Game End<<<<<<<<<<<<<<<<")
                         flag = False
         else:
-            slowprt(now, 2, 2, f"Fishing Not Running {round(pause.confi, 3)}")
+            if flag:
+                print(">>>>>>>>>>>>>>>>Fishing Game End<<<<<<<<<<<<<<<<")
+            flag = False
+            slowprt(now, 2, 10, f"Fishing Not Running")
 
 def main():
     window_title = "NIKKE"
